8adb9823042d26f1d398ca3647314a3413f953e6
SANCHEZ Nicolas <nicolas.sanchez@etu.univ-amu.fr>
2022-11-30 08:42:01 +0000

    Upload New File

---
+++ src/main/java/EntittyManager/MotoeizedFireFighterManager.java
@@ -0,0 +1,27 @@
+package entittyManager;
+
+import ground.VisitorGroundInterface;
+import util.Position;
+import grid.InterfaceVisitorPaint;
+
+import java.util.*;
+
+public class MotoeizedFireFighterManager extends FireFightersManager {
+
+
+    public MotoeizedFireFighterManager(int numberEntity, double rowCount, double colCount, FiresManager fires,List<VisitorGroundInterface> visitorGroundInterfaceList) {
+        super(numberEntity, rowCount, colCount, fires,visitorGroundInterfaceList);
+    }
+
+    @Override
+    protected Position activate_Firefighter(Position position) {
+        return just_One_Step(just_One_Step(position));
+    }
+
+
+    @Override
+    public void accept(InterfaceVisitorPaint visitor) {
+        visitor.visitMotorizedFireFighters(this);
+    }
+
+}
